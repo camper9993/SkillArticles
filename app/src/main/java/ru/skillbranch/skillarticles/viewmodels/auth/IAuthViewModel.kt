772ea499ae01ac6847bf4a09ffe93ab5d09ee060
camper9993 <31688953+camper9993@users.noreply.github.com>
2021-08-18 18:16:22 +0300

    Delete app/src/main directory

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/auth/IAuthViewModel.kt
+++
@@ -1,8 +0,0 @@
-package ru.skillbranch.skillarticles.viewmodels.auth
-
-interface IAuthViewModel {
-    /**
-     * обработка авторизации пользователя
-     */
-    fun handleLogin(login:String, pass:String, dest:Int?)
-}
--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/profile/ProfileViewModel.kt
+++
@@ -1,7 +0,0 @@
-package ru.skillbranch.skillarticles.viewmodels.profile
-
-import androidx.lifecycle.ViewModel
-
-class ProfileViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
